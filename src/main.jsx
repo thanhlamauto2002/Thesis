f6d20e1f25938a2f465a00994b270ddf4bc9053c
thanhlam <thanhlama102@gmail.com>
2024-03-14 23:51:46 +0700

    14_3

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 // import theme from '~/theme'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
@@ -12,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    <ToastContainer />
   </React.StrictMode>
 )
 
